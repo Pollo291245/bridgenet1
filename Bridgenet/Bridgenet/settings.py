4ca64bc9dfc305cfb6c137cb5d2ed71085c7423b
Pollo291245 <roaalcantara45@gmail.com>
2026-06-17 03:09:12 -0400

    Configurar variables de entorno y limpiar archivos temporales

--- Bridgenet/Bridgenet/settings.py
+++ Bridgenet/Bridgenet/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +24,12 @@ TEMPLATES_DIR= BASE_DIR / 'templates'
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kp9j=$p7gy+j7jt!d$08i@=y4q&pu2*mg8hu_$br^ow9om$i0&'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ["bridge-net.live",
-    "www.bridge-net.live", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',') if os.getenv('ALLOWED_HOSTS') else [ "127.0.0.1", "localhost"]
 
 
 # Application definition
